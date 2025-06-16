3178f641aa907b217980a67552ecf520a898cbba
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2025-06-16 15:30:21 +0100

    chore: implement homepage 2

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,7 +1,22 @@
-const CTA = () => {
-  return (
-    <div>Cta</div>
-  )
-}
+import Image from "next/image";
+import Link from "next/link";
 
-export default CTA
+const Cta = () => {
+    return (
+        <section className="cta-section">
+            <div className="cta-badge">Start learning your way.</div>
+            <h2 className="text-3xl font-bold">
+                Build and Personalize Learning TutorAgent
+            </h2>
+            <p>Pick a name, subject, voice, & personality — and start learning through voice conversations that feel natural and fun.</p>
+            <Image src="images/cta.svg" alt="cta" width={362} height={232} />
+            <button className="btn-primary">
+                <Image src="/icons/plus.svg" alt="plus" width={12} height={12}/>
+                <Link href="/companions/new">
+                    <p>Build a New TutorAgent</p>
+                </Link>
+            </button>
+        </section>
+    )
+}
+export default Cta
